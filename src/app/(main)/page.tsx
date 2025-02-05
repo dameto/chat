503f17580cca115d92b9bfff87aefced39efbe52
Damjan Nikolovski <damjan.nikolovski@yahoo.com>
2025-02-05 16:39:14 +0100

    finished home chat page design (animations required and little details)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -139,45 +139,74 @@ export default function ChatPage() {
           </div>
         </div>
 
+        {/* Chat Field */}
+        <div className="w-full h-full flex flex-col justify-end gap-1">
+          {/* Recieved */}
+          <div className="w-full flex flex-col gap-[4px]">
+            <span className="text-[15px] text-white flex flex-row gap-[6px] leading-5 align-middle pl-2">
+              @johndoe <span>•</span>
+              <span className="text-white/50">12:01 AM</span>
+            </span>
+            <p className="px-[20px] py-[14px] text-white bg-gradient-to-r from-[#3F3F3F]/[0.15] to-[#404040]/[0.15] rounded-[25px] w-max max-w-[50%]">
+              hey, joe! I’m writing because I’ve had a brand new phone
+            </p>
+          </div>
+
+          {/* Sent */}
+          <div className="w-full flex flex-col gap-[4px] items-end">
+            <span className="text-[15px] flex flex-row gap-[6px] leading-5 align-middle pr-2 text-white/50">
+              12:01 AM
+            </span>
+            <p className="px-[20px] py-[14px] text-white bg-gradient-to-r from-[#1678E8]/[0.15] to-[#1A4677]/[0.15] rounded-[25px] w-max max-w-[50%]">
+              hey, john! I’d be happy to
+            </p>
+          </div>
+        </div>
+
         {/* Message Input */}
         <div className="bg-gradient-to-r from-[#a6a6a6]/[0.01] to-[#0e0e0f] rounded-[20px] p-2 flex flex-row justify-between items-center gap-2">
           <div className="flex flex-row justify-between items-center gap-2 w-full">
-            <span className="flex items-center justify-center">
+            <button className="text-[#616169]/60 h-[35px] w-[35px] bg-gradient-to-r from-[#a6a6a6]/[0.025] to-[#0e0e0f] rounded-[20px] flex justify-center items-center cursor-pointer  hover:text-[#616169]/90 hover:from-[#a6a6a6]/[0.05] duration-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={2}
+                strokeWidth={2.5}
                 stroke="currentColor"
-                className="size-5 text-white/40 p-0"
+                className="size-4 relative left-[2px]"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
                 />
               </svg>
-            </span>
+            </button>
 
             <input
               className="bg-transparent border-transparent outline-none leading-5 p-0 align-middle placeholder-[#3C3C43]/[0.6] font-normal text-white/95 w-full"
-              placeholder="Search"
+              placeholder="Type your message here"
             />
           </div>
 
-          <button className="text-green-600/60 h-[35px] w-[35px] bg-gradient-to-r from-[#a6a6a6]/[0.025] to-[#0e0e0f] rounded-[20px] flex justify-center items-center cursor-pointer  hover:text-green-600/90 hover:from-[#a6a6a6]/[0.05] duration-200">
+          <button className="text-red-600/60 h-[35px] w-[35px] bg-gradient-to-r from-[#a6a6a6]/[0.025] to-[#0e0e0f] rounded-[20px] flex justify-center items-center cursor-pointer  hover:text-red-600/90 hover:from-[#a6a6a6]/[0.05] duration-200">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
+              strokeWidth={2.5}
               stroke="currentColor"
               className="size-5"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
+                d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z"
               />
             </svg>
           </button>
